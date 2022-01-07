6426f881fa095b0c4ad25f0b485a61373c06a85e
Roberto B <roberto.butti@gmail.com>
2022-01-07 21:05:49 +0100

    adding some tests

--- tests/StatisticTest.php
+++ tests/StatisticTest.php
@@ -2,7 +2,7 @@
 
 use HiFolks\Statistics\Statistics;
 
-it('can calculate frequencies', function () {
+it('can calculate statistics', function () {
     $s = Statistics::make(
         [98, 90, 70,18,92,92,55,83,45,95,88,76]
     );
@@ -13,7 +13,7 @@ it('can calculate frequencies', function () {
     expect($s->getInterQuartileRange())->toEqual(29.5);
 });
 
-it('can calculate frequencies again', function () {
+it('can calculate statistics again', function () {
     // https://www.youtube.com/watch?v=6z7B7ADL6Lw&ab_channel=TheMathsProf
     $s = Statistics::make(
         [3,5,4,7,5,2]
@@ -26,6 +26,33 @@ it('can calculate frequencies again', function () {
     expect($s->getMax())->toEqual(7);
     expect($s->getRange())->toEqual(5);
 });
+
+it('can calculate statistics again and again', function () {
+    // https://www.purplemath.com/modules/meanmode.htm
+    $s = Statistics::make(
+        [13, 18, 13, 14, 13, 16, 14, 21, 13]
+    );
+    expect($s->getCount())->toEqual(9);
+    expect($s->getMean())->toEqual(15);
+    expect($s->getMedian())->toEqual(14);
+    expect($s->getMode())->toEqual(13);
+    expect($s->getMin())->toEqual(13);
+    expect($s->getMax())->toEqual(21);
+    expect($s->getRange())->toEqual(8);
+
+    $s = Statistics::make(
+        [1, 2, 4, 7]
+    );
+    expect($s->getCount())->toEqual(4);
+    expect($s->getMean())->toEqual(3.5);
+    expect($s->getMedian())->toEqual(3);
+    expect($s->getMode())->toBeNull();
+    expect($s->getMin())->toEqual(1);
+    expect($s->getMax())->toEqual(7);
+    expect($s->getRange())->toEqual(6);
+});
+
+
 
 it('can strip zeros', function () {    // https://www.youtube.com/watch?v=6z7B7ADL6Lw&ab_channel=TheMathsProf
     $s = Statistics::make(

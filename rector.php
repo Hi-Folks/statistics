c7c82489a8786e87b6231a94fa8cc2b8d7d7d88d
Roberto Butti <roberto.butti@gmail.com>
2023-03-11 15:20:21 +0100

    introducing Rector

---
+++ rector.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        // __DIR__ . '/examples',
+        __DIR__ . '/src',
+        // __DIR__ . '/tests',
+    ]);
+
+    // register a single rule
+    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+
+    // define sets of rules
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_80,
+        SetList::DEAD_CODE,
+        //SetList::CODE_QUALITY,
+        //SetList::EARLY_RETURN,
+        //SetList::TYPE_DECLARATION,
+        //SetList::PRIVATIZATION
+    ]);
+};
